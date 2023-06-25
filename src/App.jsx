325d737ca1b3f60e10c29b429d1a5f3a7bf9a330
Sayyedbehzad Hashemian <Elna0016@algonquinlive.com>
2023-06-25 13:51:35 -0400

    building a schema

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import styles from './style'
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        navbar
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.flexStart}`} >
+        <div className={`${styles.boxWidth}`} >
+          main
+        </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`} >
+        <div className={`${styles.boxWidth}`} >
+          Other
+        </div>
+    </div>
+
+
+      </>
+)
 }
 
 export default App
