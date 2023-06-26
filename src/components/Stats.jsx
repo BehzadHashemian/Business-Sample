4ab4158501a0470d4fa51d94ac4e352e46724c9c
Sayyedbehzad Hashemian <Elna0016@algonquinlive.com>
2023-06-26 09:49:12 -0400

    aligning items

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -6,7 +6,7 @@ const Stats = () => {
     <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
       {stats.map((sta) => {
         return(
-          <div className= 'flex flex-1 justify-center m-3'>
+          <div className= 'flex flex-1 justify-center m-3 items-center'>
           <h1 className='text-white font-poppins font-extrabold mr-6 text-[30px] xs:text-[40px] xs:leading-[53px] leading-[43px]'>  {sta.value}</h1>
           <p className='text-gradient font-poppins text-[15px] xs:text-[20px] xs:leading-[26px] leading-[21px] ml-3 uppercase' >{sta.title}</p>
           </div>
