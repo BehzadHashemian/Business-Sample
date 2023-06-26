565948d860d11cdc666591c6aea1c8729a3aa9e4
Sayyedbehzad Hashemian <Elna0016@algonquinlive.com>
2023-06-26 10:36:16 -0400

    adding get started button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div>Button</div>
+    <button type='button' className='py-4 px-6 bg-blue-gradient font-poppins rounded-md text-primary outline-none'>
+      Get Started
+    </button>
   )
 }
 
